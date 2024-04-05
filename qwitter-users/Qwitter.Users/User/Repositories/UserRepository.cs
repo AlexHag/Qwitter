a1b8dbcd20f115025564136e97072e6ed3f657b3
AlexHag <alex.hagdahl.business@gmail.com>
2024-04-05 17:23:35 +0200

    move user repository interface

--- qwitter-users/Qwitter.Users/User/Repositories/UserRepository.cs
+++ qwitter-users/Qwitter.Users/User/Repositories/UserRepository.cs
@@ -5,6 +5,17 @@ using Qwitter.Users.User.Models;
 
 namespace Qwitter.Users.Repositories.User;
 
+public interface IUserRepository
+{
+    Task<UserEntity?> GetUserById(Guid userId);
+    Task<UserEntity?> GetUserByUsername(string username);
+    Task<UserEntity?> GetUserByEmail(string email);
+    Task<UserEntity?> GetUserByUsernameOrEmail(string usernameOrEmail);
+    Task<UserEntity> InsertUser(UserInsertModel user);
+    Task<UserEntity> UpdateUser(UserUpdateModel user);
+    Task<UserEntity> DeleteUser(Guid userId);
+}
+
 public class UserRepository : IUserRepository
 {
     private readonly AppDbContext _dbContext;
